dd084c56aeb953791eebaa5c46daee84586384e0
iamatos3 <matosian93@gmail.com>
2023-07-26 14:50:32 -0400

    Created functionality of increasing quantity for amounts and pricing
    - Created utils.js for the calculations alone

--- src/context.jsx
+++ src/context.jsx
@@ -10,27 +10,60 @@ import {
   LOADING,
   DISPLAY_ITEMS
 } from "./actions";
+import { getTotals } from "./utils";
+
+const url = 'https://www.course-api.com/react-useReducer-cart-project';
 
 const AppContext = createContext();
 
 const initialState = {
   loading: false,
-  cart: new Map(cartItems.map((item) => [item.id, item])),
+  cart: new Map(),
 };
 
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const { totalAmount, totalCost } = getTotals(state.cart);
+
   const clearCart = () => {
     dispatch({ type: CLEAR_CART });
   };
 
-  const removeItem = (id) => {
+  const remove = (id) => {
     dispatch({ type: REMOVE, payload: { id } });
   };
 
+  const increase = (id) => {
+    dispatch({ type: INCREASE, payload: { id } });
+  };
+
+  const decrease = (id) => {
+    dispatch({ type: DECREASE, payload: { id } });
+  };
+
+  const fetchData = async() => {
+    dispatch({ type: LOADING });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: DISPLAY_ITEMS, payload: { cart } });
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <AppContext.Provider value={{ ...state, clearCart, removeItem }}>
+    <AppContext.Provider
+      value={{
+        ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+        totalAmount,
+        totalCost,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
